e7dccb9dd9b7df4753719382996de749b653c50f
Jere Tofferi <jere.tofferi@gmail.com>
2021-02-14 02:53:06 +0200

    Some progress

--- vt2.py
+++ vt2.py
@@ -1,21 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
+
+from flask_wtf_polyglot import PolyglotForm #tämä tuottaa xml-yhteensopivia lomakekenttiä
+from wtforms import Form, BooleanField, StringField, validators, IntegerField, SelectField, widgets, SelectMultipleField, ValidationError
+
+import os
+SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
-counter = 0
 
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def test():
-    global counter
+    # luodaan dict
+    fields = {"size": "", "name1": "", "name2": ""}
+    errors = dict(fields)  # tekee kopion samoilla avaimilla
+    if request.method == 'POST':  # varmistetaan, että lomake on lähetetty
+        for k in errors:
+            try:
+                fields[k] = request.form[k]
+            except KeyError:
+                errors[k] = "!"
 
-    try:
-        count = int(request.args.get("count", 4))
-    except:
-        count = 0
+    class GameField(PolyglotForm):
+        size = IntegerField('Laudan koko', validators=[validators.InputRequired(), validators.NumberRange(min=1, message="Liian pieni taulukko")])
+        name1 = StringField('Pelaaja 1', validators=[validators.InputRequired(), validators.Length(min=2, message="Liian lyhyt nimi")])
+        name2 = StringField('Pelaaja 2', validators=[validators.InputRequired(), validators.Length(min=2, message="Liian lyhyt nimi")])
 
-    counter = counter + count
+    form = GameField()
+    if request.method == 'POST':
+        form.validate()
 
-    return render_template('view.html', counter=counter)
+    resp = make_response(render_template('view.xhtml', form=form))
+    resp.headers['Conent-type'] = 'application/xhtml+xml;charset=UTF-8'
+    return resp
